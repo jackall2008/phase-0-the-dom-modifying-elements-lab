c1e75539b30a453cdf5541a4857fab58b48c44c2
Julian Patterson <julian.patterson@gmail.com>
2023-01-08 23:26:57 -0600

    commit tree f38acf2272c4993c2f02be94418d1f172a0031d7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainMain = document.getElementById("main");
+
+mainMain.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'is the champion';
+
